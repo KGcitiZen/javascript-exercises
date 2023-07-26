da64bbaee042db93c0754cbd8a03e22562518e55
kgcitizen <bekturboobekov@gmail.com>
2023-07-27 05:43:46 +0600

    Add solution to 02_repeatString task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, repeatTimes) {
+  let repeatedString = '';
 
+  if (repeatTimes < 0) {
+    return 'ERROR';
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
